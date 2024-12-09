275570c476abc7d61a6376e9a6d1febf7348d41b
SamerFattah <samer.fattah37@gmail.com>
2024-12-09 18:12:25 +0200

    Update app.py

--- app.py
+++ app.py
@@ -147,7 +147,7 @@ if tabs == "Company Overview":
         """,
         unsafe_allow_html=True,
     )
-      st.image("engineer.jpg", use_column_width=True, caption="Our Corporation Headquarters")
+      st.image("https://github.com/SamerFattah/capstone-aub/blob/main/engineer.jpg", use_column_width=True, caption="Our Corporation Headquarters")
       st.markdown("</div>", unsafe_allow_html=True)
 
     # Content Section
