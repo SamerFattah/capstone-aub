2d278e6b3b01b2ed30849440a4e4bd2e5534aee4
SamerFattah <samer.fattah37@gmail.com>
2024-12-09 20:45:27 +0200

    Update app.py

--- app.py
+++ app.py
@@ -522,8 +522,8 @@ if tabs == 'Predictive Table':
         wps_table = output_table.groupby('WBS', as_index=False)['Predicted Total Hrs'].sum().reset_index(drop=True)
         # Display the tables in Streamlit
         st.subheader("Predicted Total Hours by Designation")
-        st.dataframe(designation_table.style.format(precision=0), use_container_width=True)
+        st.table(designation_table.style.format(precision=0))
         st.subheader("Predicted Total Hours by WBS")
-        st.dataframe(wps_table.style.format(precision=0), use_container_width=True)
+        st.table(wps_table.style.format(precision=0), use_container_width=True)
     else:
         st.warning("Please wait while the model is trained.")
